d827a7f9a1c2d11b8f0327d6b5cfa8163f287e74
Caseywalker <casey.walker118@gmail.com>
2021-09-18 13:20:58 -0500

    Refactoring ADO.NET methods with Dapper.

--- LinenAndBird/DataAccessLayer/BirdRepository.cs
+++ LinenAndBird/DataAccessLayer/BirdRepository.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace LinenAndBird.DataAccessLayer
 {
@@ -14,98 +15,135 @@ namespace LinenAndBird.DataAccessLayer
     internal IEnumerable<Bird> GetAll()
     {
       // connections are like the tunnel between our app and the database
-      using var connection = new SqlConnection(_connectionString);
+      //using var connection = new SqlConnection(_connectionString);
+
+      //Dapper still requires a connection to the DB
+      using var db = new SqlConnection(_connectionString);
+
+      //Query<T> is for getting results from the database and putting them into a C# type
+      var birds = db.Query<Bird>(@"SELECT * 
+                    FROM Birds");
+
+      return birds;
       //connections aren't open by default, we've gotta do that ourself
-      connection.Open();
+      //connection.Open();
 
       //This is what tells SQL what we want to do. 
-      var command = connection.CreateCommand();
-      command.CommandText = @"SELECT *
-                              FROM Birds";
+      //var command = connection.CreateCommand();
+      //command.CommandText = @"SELECT *
+                              //FROM Birds";
 
       //execute reader is to get all results of the query
-      var reader = command.ExecuteReader();
+      //var reader = command.ExecuteReader();
 
-      var birds = new List<Bird>();
+      //var birds = new List<Bird>();
 
       // data readers only get one row from the results at a time, need to use a while statement to get all
-      while (reader.Read())
-      {
+      //while (reader.Read())
+      //{
         //Mapping data from the relational model to the object model
-        var bird = MapFromReader(reader);
+        //var bird = MapFromReader(reader);
 
         //each bird goes into the list to return
-        birds.Add(bird);
-      }
-      return birds;
+        //birds.Add(bird);
+      //}
+      //return birds;
       //return _birds;
     }
 
     internal void Remove(Guid id)
     {
-      using var connection = new SqlConnection(_connectionString);
+      //using var connection = new SqlConnection(_connectionString);
+      using var db = new SqlConnection(_connectionString);
+      var sql = @"Delete
+                  FROM Birds
+                  WHERE Id = @id";
 
-      connection.Open();
+      db.Execute(sql, new { id });
 
-      var command = connection.CreateCommand();
-      command.CommandText = @"Delete
-                              FROM Birds
-                              WHERE Id = @id";
+      //connection.Open();
 
-      command.Parameters.AddWithValue("id", id);
-      command.ExecuteReader();
+      //var command = connection.CreateCommand();
+      //command.CommandText = @"Delete
+      //                        FROM Birds
+      //                        WHERE Id = @id";
+
+      //command.Parameters.AddWithValue("id", id);
+      //command.ExecuteReader();
     }
 
     internal Bird Update(Guid id, Bird bird)
     {
-      using var connection = new SqlConnection(_connectionString);
+      //using var connection = new SqlConnection(_connectionString);
+      using var db = new SqlConnection(_connectionString);
+      var sql = @"UPDATE Birds
+                SET Type = @type,
+                 Color = @color,
+                 Size = @size,
+                 Name = @name
+                 OUTPUT inserted.*
+                 WHERE Id = @id";
+      bird.Id = id;
 
-      connection.Open();
+      var updatedBird = db.QuerySingleOrDefault(sql, bird);
 
-      var command = connection.CreateCommand();
-      command.CommandText = @"UPDATE Birds
-                            SET Type = @type,
-	                          Color = @color,
-	                          Size = @size,
-	                          Name = @name
-                            OUTPUT inserted.*
-                            WHERE Id = @id";
+      return updatedBird;
 
-      command.Parameters.AddWithValue("color", bird.Color);
-      command.Parameters.AddWithValue("size", bird.Size);
-      command.Parameters.AddWithValue("name", bird.Name);
-      command.Parameters.AddWithValue("type", bird.Type);
-      command.Parameters.AddWithValue("id", id);
+      //connection.Open();
 
-      var reader = command.ExecuteReader();
+      //var command = connection.CreateCommand();
+      //command.CommandText = @"UPDATE Birds
+      //                      SET Type = @type,
+      //                     Color = @color,
+      //                     Size = @size,
+      //                     Name = @name
+      //                      OUTPUT inserted.*
+      //                      WHERE Id = @id";
 
-      if (reader.Read())
-      {
-        return MapFromReader(reader);
-      }
+      //command.Parameters.AddWithValue("color", bird.Color);
+      //command.Parameters.AddWithValue("size", bird.Size);
+      //command.Parameters.AddWithValue("name", bird.Name);
+      //command.Parameters.AddWithValue("type", bird.Type);
+      //command.Parameters.AddWithValue("id", id);
 
-      return null;
+      //var reader = command.ExecuteReader();
+
+      //if (reader.Read())
+      //{
+      //  return MapFromReader(reader);
+      //}
+
+      //return null;
     }
 
     internal void Add(Bird newBird)
     {
-      using var connection = new SqlConnection(_connectionString);
+      var db = new SqlConnection(_connectionString);
 
-      connection.Open();
+      var sql = @"insert into birds(Type, Color, Size, Name)
+                            output inserted.Id
+                            values (@Type, @Color, @Size, @Name)";
 
-      var command = connection.CreateCommand();
-      command.CommandText = @"insert into birds(Type, Color, Size, Name)
-                              output inserted.Id
-                              values (@Type, @Color, @Size, @Name)";
 
-      command.Parameters.AddWithValue("Type", newBird.Type);
-      command.Parameters.AddWithValue("Color", newBird.Color);
-      command.Parameters.AddWithValue("Size", newBird.Size);
-      command.Parameters.AddWithValue("Name", newBird.Name);
+      var id = db.ExecuteScalar<Guid>(sql, newBird);
+      newBird.Id = id;
+      //using var connection = new SqlConnection(_connectionString);
 
-      var newId = (Guid)command.ExecuteScalar();
+      //connection.Open();
 
-      newBird.Id = newId;
+      //var command = connection.CreateCommand();
+      //command.CommandText = @"insert into birds(Type, Color, Size, Name)
+      //                        output inserted.Id
+      //                        values (@Type, @Color, @Size, @Name)";
+
+      //command.Parameters.AddWithValue("Type", newBird.Type);
+      //command.Parameters.AddWithValue("Color", newBird.Color);
+      //command.Parameters.AddWithValue("Size", newBird.Size);
+      //command.Parameters.AddWithValue("Name", newBird.Name);
+
+      //var newId = (Guid)command.ExecuteScalar();
+
+      //newBird.Id = newId;
 
       //newBird.Id = Guid.NewGuid();
 
@@ -114,29 +152,37 @@ namespace LinenAndBird.DataAccessLayer
 
     internal Bird GetById(Guid birdId)
     {
-      using var connection = new SqlConnection(_connectionString);
+      //using var connection = new SqlConnection(_connectionString);
+      using var db = new SqlConnection(_connectionString);
 
-      connection.Open();
+      var sql = @"SELECT *
+                  FROM Birds
+                  WHERE id= @id";
 
-      var command = connection.CreateCommand();
-      command.CommandText = $@"SELECT *
-                               FROM Birds
-                                WHERE id= @id";
+      var bird = db.QuerySingleOrDefault<Bird>(sql, new { id = birdId });
 
-      //parameterization prevents sql injection
-      command.Parameters.AddWithValue("id", birdId);
+      return bird;
+      //connection.Open();
 
-      //execute reader is to get all results of the query
-      var reader = command.ExecuteReader();
+      //var command = connection.CreateCommand();
+      //command.CommandText = $@"SELECT *
+      //                         FROM Birds
+      //                          WHERE id= @id";
 
-      // data readers only get one row from the results at a time, need to use a while statement to get all
-      if(reader.Read())
-      {
-        return MapFromReader(reader);
-      }
-      return null;
+      ////parameterization prevents sql injection
+      //command.Parameters.AddWithValue("id", birdId);
 
-      //return _birds.FirstOrDefault(bird => bird.Id == birdId);
+      ////execute reader is to get all results of the query
+      //var reader = command.ExecuteReader();
+
+      //// data readers only get one row from the results at a time, need to use a while statement to get all
+      //if(reader.Read())
+      //{
+      //  return MapFromReader(reader);
+      //}
+      //return null;
+
+      ////return _birds.FirstOrDefault(bird => bird.Id == birdId);
     }
 
     Bird MapFromReader(SqlDataReader reader)
